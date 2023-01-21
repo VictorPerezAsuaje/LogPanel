a8b40d76981c37dbffa84c377e9174db95f70645
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2023-01-21 11:14:32 +0100

    Added LogPanelViews folders removed by gitignore

---
+++ LogPanelViews/Areas/Log/Controllers/ExceptionsController.cs
@@ -0,0 +1,36 @@
+﻿using LogPanelEntities.Entities;
+using LogPanelEntities.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogPanelViews.Controllers;
+
+[Area("Log")]
+public class ExceptionsController : Controller
+{
+    ILogRepository _logRepo;
+    IClientLogDbConfig dbConfig;
+    public ExceptionsController(ILogRepository logRepository, IClientLogDbConfig databaseConfig)
+    {
+        _logRepo = logRepository;
+        dbConfig = databaseConfig;
+    }
+
+    public IActionResult Index(string? clientSelected = null)
+    {
+        if(clientSelected == null)
+            clientSelected = dbConfig.GetAllClients().FirstOrDefault().Name;
+
+        return View(_logRepo.GetLogsByClientName(clientSelected, dbConfig));
+    }
+
+    [HttpGet]
+    [Route("[area]/[controller]/[action]")]
+    public IActionResult PollClientLogs(string clientSelected)
+    {
+        if (clientSelected == null)
+            clientSelected = dbConfig.GetAllClients().FirstOrDefault().Name;
+
+        List<Log> logs = _logRepo.GetLogsByClientName(clientSelected, dbConfig);
+        return PartialView("_ListadoLogs", logs);
+    }
+}
